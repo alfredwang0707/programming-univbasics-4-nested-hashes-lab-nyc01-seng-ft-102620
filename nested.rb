f26591cb57b72ec049cf3c68c98e5fb744948bf0
Alfred Wang <alfredwang0707@gmail.com>
2020-10-06 19:30:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][0]
+programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def changing_alan
